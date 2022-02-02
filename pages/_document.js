e9fc93c9ef9c09e9c7137403d27a2f361146aaa9
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-02-02 22:00:13 +0100

    add new fonts

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;700;800&family=Courier+Prime:ital,wght@0,400;0,700;1,400&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

0c9ef7333acc5a8f9fab9ae2432849b43bba27fe
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-02-02 16:27:24 +0100

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+const Layout = ({ children }) => {
+  return (
+    <>
+      {/* <span>Her kommer navbar</span> */}
+      {children}
+      {/* <span>Her kommer footer</span> */}
+    </>
+  );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <ThemeProvider enableSystem={true} attribute="class">
+    // remove comment and remove provider under when added button for switching darkmode
+    <ThemeProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
